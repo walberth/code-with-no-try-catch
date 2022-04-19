ef064c32f6cdadd115b2cdb2effcf9f714b4795b
Walbert Gutierrez Telles <w.felipe.gutierrez@gmail.com>
2022-04-19 14:16:27 -0500

    proyectos del blog

---
+++ UnitTestMoqNetCoreDemo/RepositoryLayer/PersonRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RepositoryLayer
+{
+    using System.Collections.Generic;
+    using RepositoryLayerInterface;
+
+    public class PersonRepository : IPersonRepository
+    {
+        public IEnumerable<string> PersonNames() {
+            var nombres = new List<string>();
+
+            nombres.Add("Walberth");
+            nombres.Add("Angela");
+            nombres.Add("otro Nombre");
+            nombres.Add("otro nombre más");
+
+            return nombres;
+        }
+
+        public string GetPersonCompleteName(string name) {
+            return "Walberth Gutierrez Telles";
+        }
+    }
+}
---
+++ UnitTestMoqNetCoreDemo/RepositoryLayerInterface/IPersonRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepositoryLayerInterface
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public interface IPersonRepository {
+        IEnumerable<string> PersonNames();
+        string GetPersonCompleteName(string name);
+    }
+}
